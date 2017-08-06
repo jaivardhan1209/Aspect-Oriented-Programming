95546442362fdd461cfb0f59d48a4629c563cc86
jaivardhan1209 <jaivardhan1209@gmail.com>
2017-08-07 00:27:57 +0530

    Aspect oriented programming
    Aop use for interceptor and can mostly use as in memory caching and
    loggin.

---
+++ MyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP
+{
+    public class MyType : IMyType
+    {
+        public void DoSomeThing(string str, int i)
+        {
+            Console.WriteLine(string.Format("MyType.Dosomething execution using '{0} and {1}",str,i));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resolve.Initialize();
+            var myType = ResolveEventArgs.For<IMyType>();
+
+            myType.DoSomething("testString", 1);
+
+            Console.ReadKey();
+        }
+    }
+}
